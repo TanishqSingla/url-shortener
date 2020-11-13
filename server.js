704cfe26e3170e01b5be1672af9e7364a52e4bbc
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-14 04:09:17 +0530

    linking database

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const shortUrl = require("./shortUrl");
+const shortenUrl = require("./shortUrl");
+
 const app = express();
+
+mongoose.connect("mongodb://localhost/shortenUrl", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
@@ -7,6 +16,11 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/shortenUrl", async (req, res) => {
+  await shortUrl.create({ full: req.full });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () =>
